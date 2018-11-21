36a400c9f85dbcd3887b0e82e7263c2df1fbbe65
laure <laure.souce@telecom-bretagne.eu>
2018-11-21 22:04:22 +0100

    ajout de methode et determination des codes pour les messages

--- Node.py
+++ Node.py
@@ -1,4 +1,6 @@
 from tree import tree
+import string
+import pika
 
 
 class Node :
@@ -8,8 +10,10 @@ class Node :
             self.neighbors = tree[id]["neighbors"]
             if id == "A" :
                 self.holder = "A"
+                self.send(".".join(self.neighbors), "I")
             else:
                 self.holder = ""
+
 
             #True if your using the resource False if not --> initialized at False
             #Can be true if and only if holder=self.id
@@ -18,14 +22,17 @@ class Node :
             #contains the ids of all the node from whom the node received a request that was not satisfied
             self.requestQueue = []
 
-            #True when non priviledged holder has send a request message to the current holder. False otherwise.
+            #True when I am the non priviledged holder and I have already sent a request message to the current holder.
+            # False otherwise.
             self.asked = False
 
 
 
         else:
             raise Exception("Id not valid")
-
+    def passPriviledge(self):
+        if self.requestQueue[0] != self.id:
+            receiver = self.requestQueue.pop()
+            self.send(receiver,P
 
 node = Node("A")
-print(node.neighbors)
