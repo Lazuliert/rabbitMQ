356efc381ed6bd2bd1bb0f2f08520f2e47e4a4fb
laure <laure.souce@telecom-bretagne.eu>
2018-11-21 18:58:43 +0100

    modification de node pour que tous les champs propres à un node soient présents

--- Node.py
+++ Node.py
@@ -6,6 +6,23 @@ class Node :
         if id in ["A", "B", "C", "D", "E", "F"]:
             self.id = id
             self.neighbors = tree[id]["neighbors"]
+            if id == "A" :
+                self.holder = "A"
+            else:
+                self.holder = ""
+
+            #True if your using the resource False if not --> initialized at False
+            #Can be true if and only if holder=self.id
+            self.using = False
+
+            #contains the ids of all the node from whom the node received a request that was not satisfied
+            self.requestQueue = []
+
+            #True when non priviledged holder has send a request message to the current holder. False otherwise.
+            self.asked = False
+
+
+
         else:
             raise Exception("Id not valid")
 
