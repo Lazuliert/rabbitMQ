a3d48defb0557ff018979b0399fc4565e4f04d22
laure <laure.souce@telecom-bretagne.eu>
2018-11-21 18:36:28 +0100

    ajout config tree

---
+++ tree.py
@@ -0,0 +1,20 @@
+tree = {
+    "A":{
+        "neighbors" : ["B", "C", "D"]
+    },
+    "B":{
+        "neighbors" : ["A"]
+    },
+    "C":{
+        "neighbors" : ["A"]
+    },
+    "D": {
+        "neighbors" : ["A", "F", "E"]
+    },
+    "E": {
+        "neighbors" : ["D"]
+    },
+    "F": {
+        "neighbors" : ["D"]
+    }
+}
