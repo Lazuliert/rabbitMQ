ba6c742056f174b3b6b69dfd8b8406ae35d45b47
lducerf <ladutreiz@gmail.com>
2018-11-24 22:43:07 +0100

    les prints sont en francais maintenant

--- Node.py
+++ Node.py
@@ -15,12 +15,6 @@ class Node(threading.Thread):
         if holder_id in ["A", "B", "C", "D", "E", "F"]:
             self.id = holder_id
             self.neighbors = tree[holder_id]["neighbors"]
-            if holder_id == "A" :
-                self.holder = "A"
-                self.send_message(".".join(self.neighbors), "I")
-            else:
-                self.holder = ""
-
 
             #True if your using the resource False if not --> initialized at False
             #Can be true if and only if holder=self.id
@@ -37,13 +31,17 @@ class Node(threading.Thread):
             self.create_queue()
 
             self.privilege=None
-
             self.stopped = False
             self.recovering = False
             self.holder_advices={} # store "Am I the holder for [sender] ?"
             self.asked_advices={}  # store "Is asked true for [sender] ?"
             self.rq_in_advices={}  # store "Am I in the request queue for [sender] ?"
 
+            if holder_id == "A" :
+                self.holder = "A"
+                self.send_message(".".join(self.neighbors), "I")
+            else:
+                self.holder = ""
 
 
         else:
@@ -54,10 +52,10 @@ class Node(threading.Thread):
     """
     def run(self):
         """This function is blocking"""
-        print(" *** Node " + self.id + " waiting for messages .To exit press CTRL+C")
+        #print(" *** Node " + self.id + " waiting for messages .To exit press CTRL+C")
 
         def callback(obj, ch, method, properties, body):
-            print(" [x] routing_key ; body %r:%r" % (method.routing_key, body))
+            #print(" [x] routing_key ; body %r:%r" % (method.routing_key, body))
             obj.receive_manager(body)
 
         self.channel.basic_consume(lambda ch, method, properties, body: callback(self, ch, method, properties, body),
@@ -70,7 +68,7 @@ class Node(threading.Thread):
     """
     def status_printer(self, status_message =""):
         if status_message != "":
-            status_message= "Status of node " + self.id
+            status_message= "Statut du noeud " + self.id
         print(status_message)
         print("holder : " + self.holder)
         print("request_q : " + str(self.request_queue))
@@ -82,65 +80,66 @@ class Node(threading.Thread):
     fonction appelée quand il reçoit un message et reroot vers la bonne méthode
     """
     def receive_manager(self, message):
-        print("#####Message Received !#####")
+        #print("#####Message Received !#####")
         message = message.decode('UTF-8')
         sender = message[0:-1]
         mtype = message[-1]
-        print("sender" + sender)
-        print("mtype" + mtype)
+        #print("sender" + sender)
+        #print("mtype" + mtype)
+        print("-->[] Le noeud " + self.id + " a recu le message " + mtype + " du noeud " + sender)
         if self.stopped == False :
             if self.recovering == False:
                 #self.status_printer()
                 if mtype =="I":
-                    print("INITIALIZE message from node " + sender + " received")
+                    #print("INITIALIZE message du noeud " + sender + " received")
                     self.initialize(sender)
                 elif mtype =="P":
-                    print("PRIVILEGE message from node " + sender + " received")
+                    #print("PRIVILEGE message from node " + sender + " received")
                     self.holder = self.id
                     self.assign_privilege()
                     self.make_request()
                 elif mtype =="Q":
-                    print("REQUEST message from node " + sender + " received")
+                    #print("REQUEST message from node " + sender + " received")
                     self.request_queue.append(sender)
                     self.assign_privilege()
                     self.make_request()
                 elif mtype =="S":
-                    print("RESTART message from node " + sender + " received")
+                    #print("RESTART message from node " + sender + " received")
                     self.make_advice(sender)
                 elif mtype in ['W', 'X,', 'Y', 'Z']:
                     print("ADVISE Message received. This is not normal")
 
                 else:
-                    print("I DO NOT KNOW")
+                    print("Message inconnu")
                 #print("New status :")
                 #self.status_printer()
                 #print("#####End of message treatment#####")
             else:
                 if mtype =="I":
-                    print("INITIALIZE message from node " + sender + " received")
+                    #print("INITIALIZE message from node " + sender + " received")
                     self.initialize(sender)
                 elif mtype =="P":
-                    print("PRIVILEGE message from node " + sender + " received")
+                    #print("PRIVILEGE message from node " + sender + " received")
                     self.holder = self.id
                     #self.assign_privilege()
                     #self.make_request()
                 elif mtype =="Q":
-                    print("REQUEST message from node " + sender + " received")
+                    #print("REQUEST message from node " + sender + " received")
                     self.request_queue.append(sender)
                     #self.assign_privilege()
                     #self.make_request()
                 elif mtype =="S":
                     print("RESTART message from node " + sender + " received")
                 elif mtype in ['W', 'X', 'Y', 'Z']:
-                    print("Advice message :" +mtype)
+                    #print("Advice message :" +mtype)
                     self.store_advice(sender, mtype)
                 else:
-                    print("I DO NOT KNOW")
+                    print("Message inconnu")
                 #print("New status :")
                 #self.status_printer()
                 #print("#####End of message treatment#####")
         else :
-            print("The node " + self.id + " is currently stopped. It has ignored this message")
+            print("Le noeud " + self.id + " est actuellement arrete. Il ignorera ce message")
 
     def make_request(self):
         if (self.holder != self.id) & (self.request_queue != []) & (self.asked == False):
@@ -175,10 +174,10 @@ class Node(threading.Thread):
             self.holder_advices[sender]= sender
             self.rq_in_advices[sender] = True
         else:
-            print("Unknown advice")
+            print("ADVISE inconnu")
 
         if len(self.holder_advices) == len(self.neighbors): # All advices received
-            print("Reconstructing data...")
+            print("Reconstruction des donnees du noeud "+self.id+" ...")
             if self.holder == "": # Je n'ai pas recu le message PRIVILEGE entre temps
                 holder_flag = True
                 for key in self.holder_advices:
@@ -208,7 +207,7 @@ class Node(threading.Thread):
             # de son asked. Si son asked est vrai, alors il veut le privilege
             # et je le met dans ma liste de requetes
             self.recovering = False
-            self.status_printer("Status after recovering")
+            #self.status_printer("Status after recovering")
             self.asked_advices = {}
             self.holder_advices = {}
             self.rq_in_advices = {}
@@ -270,7 +269,7 @@ class Node(threading.Thread):
         self.channel.queue_bind(exchange='topic_KRaymond',
                                 queue = self.queue_name,
                                 routing_key=binding_key)
-        print(" *** Node " + self.id + " declared queue with binding key " + binding_key + " .")
+        print(" *** Le Noeud " + self.id + " a declare la queue avec la binding key " + binding_key + " .")
 
 
     def send_message(self, destination, message):
@@ -285,7 +284,7 @@ class Node(threading.Thread):
         channel.basic_publish(exchange='topic_KRaymond',
                               routing_key=destination,
                               body=payload)
-        print(" [x] Sent %r:%r" % (destination, payload))
+        print("[]--> Le noeud " + self.id + " a envoye " + destination+ ":" +payload)
         connection.close()
 
     def exit_critical_section(self):
@@ -316,33 +315,35 @@ nodes["A"].start()
 
 
 while True:
-    request = input("would you like to do something?")
+    request = input("Que voulez vous faire?")
     if request in ["A", "B", "C", "D", "E", "F"]:
         node = nodes[request]
-        node.status_printer()
         if node.stopped :
-            print("This node is stopped. It will do nothing")
+            print("Ce noeud est arrete. Il ne fera rien")
         elif node.using:
-            print(request + " will know stop using the privilege")
+            print(request + " va arreter d'utiliser le privilege")
 
             node.exit_critical_section()
         else:
-            node.status_printer()
-            print(request + " will now ask for the privilege for himself" )
+            #node.status_printer()
+            print(request + " va maintenant demander le privilege pour lui" )
             node.make_wish()
 
 
     elif request[0] == "C" and len(request)==3:
         holder_id = request[2]
         node = nodes[holder_id]
-        node.status_printer()
         if node.stopped == False:
-            print("node " + holder_id + " will now be stopped")
+            print("node " + holder_id + " va maintenant s'arreter")
             node.stop()
         else:
-            print("node " + holder_id + " will now restart")
+            print("node " + holder_id + " va maintenant redemarrer")
             node.restart()
 
+    elif request[0] == "I" and len(request)==3:
+        holder_id = request[2]
+        node = nodes[holder_id]
+        node.status_printer("Statut du noeud "+holder_id)
     else:
         print("Invalid command")
 
